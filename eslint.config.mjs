92dcd1e4cf9235e9fef3d48b0262c721be59a576
Thiago Cruz <thiagocruz.eu@gmail.com>
2025-04-01 14:23:17 -0300

    chore: config eslint and prettier

---
+++ eslint.config.mjs
@@ -0,0 +1,27 @@
+import { defineConfig } from 'eslint/config';
+import globals from 'globals';
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs,ts}'],
+    languageOptions: { globals: globals.node },
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { argsIgnorePattern: 'next' },
+      ],
+      'no-console': 'error',
+    },
+  },
+  {
+    files: ['**/*.{js,mjs,cjs,ts}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+  },
+  tseslint.configs.recommended,
+  {
+    ignores: ['node_modules/*', 'dist/*'],
+  },
+]);
