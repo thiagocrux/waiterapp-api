3461d116ef5312c1eaf36b4eeff4e98ec41494d4
Thiago Cruz <thiagocruz.eu@gmail.com>
2025-04-03 09:41:01 -0300

    feat: add initial database config

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,17 @@
+/* eslint-disable no-console */
 import express from 'express';
+import mongoose from 'mongoose';
 
-const app = express();
+const PORT = 3001;
 
-app.listen(3001, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Server running on http://localhost:3001`);
-});
+mongoose
+  .connect('mongodb://localhost:27017')
+  .then(() => {
+    const app = express();
+
+    app.listen(PORT, () => {
+      console.log('Database running on mongodb://localhost:27017');
+      console.log(`Server running on http://localhost:${PORT}`);
+    });
+  })
+  .catch((error) => console.log(`[mongodb] Error: `, error));
