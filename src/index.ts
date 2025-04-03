dfca827d4d000795fa44e4704664e839df8b11aa
Thiago Cruz <thiagocruz.eu@gmail.com>
2025-04-02 23:28:15 -0300

    chore: config express

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
-// eslint-disable-next-line no-console
-console.log('Hello, world!');
+import express from 'express';
+
+const app = express();
+
+app.listen(3001, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Server running on http://localhost:3001`);
+});
