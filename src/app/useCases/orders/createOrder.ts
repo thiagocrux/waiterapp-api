e19cc9437761b9faf11b5fe0736fd7abb419a5af
Thiago Cruz <thiagocruz.eu@gmail.com>
2025-04-30 01:09:07 -0300

    feat: implement websocket listener for order creation

--- src/app/useCases/orders/createOrder.ts
+++ src/app/useCases/orders/createOrder.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import { io } from '../../..';
 
 import { Order } from '../../models';
 
@@ -6,6 +7,8 @@ export async function createOrder(request: Request, response: Response) {
   try {
     const { table, products } = request.body;
     const order = await Order.create({ table, products });
+    const orderDetails = await order.populate('products.product');
+    io.emit('orders@new', orderDetails);
     response.status(201).json(order);
   } catch (error) {
     response.status(500).json({ error });
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,21 @@
 import 'dotenv/config';
 import express from 'express';
 import mongoose from 'mongoose';
+import http from 'node:http';
 import path from 'node:path';
+import { Server } from 'socket.io';
 
 import { router } from './router';
 import { logger } from './utils';
 
-const PORT = 3001;
+const app = express();
+const server = http.createServer(app);
+export const io = new Server(server);
 
 mongoose
   .connect('mongodb://localhost:27017')
   .then(() => {
-    const app = express();
+    const PORT = 3001;
 
     app.use((request, response, next) => {
       response.setHeader('Access-Control-Allow-Origin', '*');
@@ -28,7 +32,7 @@ mongoose
     app.use(express.json());
     app.use(router);
 
-    app.listen(PORT, () => {
+    server.listen(PORT, () => {
       logger.info('Database running on mongodb://localhost:27017');
       logger.info(`Server running on http://localhost:${PORT}`);
     });
